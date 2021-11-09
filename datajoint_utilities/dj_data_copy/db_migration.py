bda265362d55e418e1b88f4b3c95e1d7d1bbe80e
Thinh Nguyen <thinh@vathes.com>
2021-11-08 18:20:22 -0600

    minor updates in docstring

--- datajoint_utilities/dj_data_copy/db_migration.py
+++ datajoint_utilities/dj_data_copy/db_migration.py
@@ -9,8 +9,7 @@ Utility for same connection migration of data between schema and table
 
 def migrate_schema(origin_schema, destination_schema):
     """
-    Data migration from all tables from origin_schema to destination_schema
-    Migration approach for DataJoint 0.12.7
+    Data migration from all tables from `origin_schema` to `destination_schema`, in topologically sorted order
     """
     total_to_transfer_count = 0
     total_transferred_count = 0
