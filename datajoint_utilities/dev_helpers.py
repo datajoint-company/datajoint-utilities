9b1302b9e163131f481d02b4c75e682e67f8ab75
Chris Broz <CBroz@datajoint.com>
2022-06-07 10:49:54 -0500

    Check list reducing during while loop

--- datajoint_utilities/dev_helpers.py
+++ datajoint_utilities/dev_helpers.py
@@ -1,5 +1,5 @@
 import datajoint as dj
-from pymysql import  OperationalError
+from pymysql import OperationalError
 
 """
 Dev helper functions. Chris Brozdowski <CBroz@datajoint.com>
@@ -9,28 +9,29 @@ Dev helper functions. Chris Brozdowski <CBroz@datajoint.com>
 
 
 def list_schemas_prefix(prefix):
-    """ Returns list of schemas with a specific prefix """
+    """Returns list of schemas with a specific prefix"""
     return [s for s in dj.list_schemas() if s.startswith(prefix)]
 
 
-def drop_schemas(prefix=None, dry_run=True, force_drop=False, max_attempts=10):
+def drop_schemas(prefix=None, dry_run=True, force_drop=False):
     """
-    Cycles through schemas with specific prefix. If not dry_run, drops the schemas 
+    Cycles through schemas with specific prefix. If not dry_run, drops the schemas
     from the database. Saves time figuring out the correct order for dropping schemas.
 
     :param prefix: Optional. If not specified, uses dj.config prefix
     :param dry_run: Optional, default True. If True, returns list would attempt to drop
-    :param force_drop: Optional, default False. Passed to `schema.drop()`. 
+    :param force_drop: Optional, default False. Passed to `schema.drop()`.
                        If True, skips the standard confirmation prompt.
-    :param max_attempts: Optional, default 10. Max attempts to drop relevant schemas
     """
     if not prefix:
         try:
             prefix = dj.config["custom"]["database.prefix"]
         except KeyError:
-            raise NameError("No prefix found in dj.config[\"custom\"]"
-                           + "[\"database.prefix\"]\n"
-                           + "Please add a prefix with drop_schemas(prefix=<prefix>)")
+            raise NameError(
+                'No prefix found in dj.config["custom"]'
+                + '["database.prefix"]\n'
+                + "Please add a prefix with drop_schemas(prefix=<prefix>)"
+            )
 
     schemas_with_prefix = list_schemas_prefix(prefix)
 
@@ -38,7 +39,8 @@ def drop_schemas(prefix=None, dry_run=True, force_drop=False, max_attempts=10):
         print("\n".join(schemas_with_prefix))
 
     else:
-        while schemas_with_prefix and max_attempts > 0:
+        while schemas_with_prefix:
+            n_schemas_initial = len(schemas_with_prefix)
             for schema_name in schemas_with_prefix:
                 try:
                     dj.schema(schema_name).drop(force=force_drop)
@@ -47,4 +49,8 @@ def drop_schemas(prefix=None, dry_run=True, force_drop=False, max_attempts=10):
                 else:
                     schemas_with_prefix.remove(schema_name)
                     print(schema_name)
-            max_attempts -= 1
+            assert n_schemas_initial != len(
+                schemas_with_prefix
+            ), f"Could not drop any of the following schemas:\n" + "\n".join(
+                schemas_with_prefix
+            )
