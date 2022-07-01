965c8c8b5afb86babacf5c51ea3a05dccb75a7b6
Thinh Nguyen <thinh@vathes.com>
2022-07-01 07:32:52 -0500

    Update email_notifier.py

--- datajoint_utilities/dj_notification/notifier/email_notifier.py
+++ datajoint_utilities/dj_notification/notifier/email_notifier.py
@@ -10,7 +10,7 @@ class MailgunEmailNotifier(Notifier):
         self.request_url = f'https://api.mailgun.net/v3/{mailgun_domain_name}/messages'
         self.body = {
             "from": f"{sender_name} <{sender_email}>",
-            "to": receiver_emails
+            "bcc": receiver_emails
         }
 
     def notify(self, title, message):
