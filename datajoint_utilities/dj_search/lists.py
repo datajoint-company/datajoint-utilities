488b1eeffefb6890741f14b8ccaac65ac367c330
CBroz1 <CBrozdowski@yahoo.com>
2023-10-31 12:07:53 -0500

    Add func to list all tables

--- datajoint_utilities/dj_search/lists.py
+++ datajoint_utilities/dj_search/lists.py
@@ -9,8 +9,10 @@ Development helper functions.
                    that they could be dropped, to avoid foreign key constraints
 """
 
+BUILT_INS = ["information_schema", "performance_schema", "sys", "mysql"]
 
-def _list_schemas_prefix(prefix, connection=None):
+
+def _list_schemas_prefix(prefix=None, connection=None):
     """Returns list of schemas with a given prefix. Helper function, Not for common use.
 
     Parameters
@@ -18,7 +20,13 @@ def _list_schemas_prefix(prefix, connection=None):
     prefix: when retruning a list of schemas, restrict by this prefix
     connection: Optional parameter passed to datajoint.list_schemas
     """
-    return [s for s in dj.list_schemas(connection=connection) if s.startswith(prefix)]
+    if prefix is None:
+        prefix = ""
+    return [
+        s
+        for s in dj.list_schemas(connection=connection)
+        if s.startswith(prefix) and s not in BUILT_INS
+    ]
 
 
 def list_drop_order(prefix):
@@ -38,28 +46,80 @@ def list_drop_order(prefix):
             depends_on[upstream].append(schema)
     drop_list = []  # ordered list to drop
     while depends_on:
-        drop_list.extend(k for k, v in depends_on.items() if not v)  # empty is dropable
-        depends_on = {k: v for k, v in depends_on.items() if v}  # remove from dict
+        drop_list.extend(
+            k for k, v in depends_on.items() if not v
+        )  # empty is dropable
+        depends_on = {
+            k: v for k, v in depends_on.items() if v
+        }  # remove from dict
         for schema in depends_on:
             # Filter out items already in drop list
-            depends_on[schema] = [s for s in depends_on[schema] if s not in drop_list]
+            depends_on[schema] = [
+                s for s in depends_on[schema] if s not in drop_list
+            ]
 
     return drop_list
 
 
-def drop_schemas(prefix, dry_run=True, ordered=False, force_drop=False):
+def list_tables(
+    schema_prefix="",
+    table_prefix="",
+    ordered=False,
+    attribute=None,
+    connection=None,
+):
+    """Return list of full table names given schema/table prefixes.
+
+    :param schema_prefix: Optional, default all schemas.
+    :param table_prefix: Optional, default all tables.
+    :param ordered: Optional, default False. If True, returns in schema drop
+        order to approx dependency order.
+    :param attribute: Optional, default no restriction. Optionally filters
+        tables by those that include the attribute.
+    """
+    if connection is None:
+        connection = dj.conn()
+
+    if ordered:
+        schemas_with_prefix = list_drop_order(schema_prefix)
+    else:
+        schemas_with_prefix = _list_schemas_prefix(schema_prefix)
+
+    table_list = [
+        f"`{s}`.`{t}`"
+        for s in schemas_with_prefix
+        for t in dj.Schema(s).list_tables()
+        if t.startswith(table_prefix)
+    ]
+
+    if attribute:
+        table_list = [
+            t
+            for t in table_list
+            if attribute in dj.FreeTable(connection, t).heading.attributes
+        ]
+
+    return table_list
+
+
+def drop_schemas(prefix=None, dry_run=True, ordered=False, force_drop=False):
     """
     Cycles through schemas with specific prefix. If not dry_run, drops the schemas
     from the database. Saves time figuring out the correct order for dropping schemas.
 
-    :param prefix: If None, uses dj.config prefix
+    :param prefix: If None, uses dj.config prefix. For all, use prefix=''.
     :param dry_run: Optional, default True. If True, prints list of schemas with prefix.
     :param ordered: Optional, default False. If True, takes time to decide drop order
                     before printing or printing & dropping. Slow process.
     :param force_drop: Optional, default False. Passed to `schema.drop()`.
                        If True, skips the standard confirmation prompt.
+
+    Example:
+        from datajoint_utilities.dj_search import drop_schemas;
+        prefix='lfp_mer';
+        drop_schemas(prefix, force_drop=True, dry_run=False)
     """
-    if not prefix:
+    if prefix is None:
         try:
             prefix = dj.config["custom"]["database.prefix"]
         except KeyError:
